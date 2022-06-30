4b33b64d41b5285d2700523ea4dff467547369e6
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-06-29 23:43:05 -0400

    add add_customer to controller, service and dao layer, add invalidParameterExceptionError and check DOB helper function

--- controller/customer_controller.py
+++ controller/customer_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from model.customer import Customer
 from service.customer_service import CustomerService
 from exception.customer_not_found import CustomerNotFoundError
+from exception.invalid_parameter import InvalidParameterError
 
 cc = Blueprint('customer_controller', __name__)
 customer_service = CustomerService()
@@ -10,21 +11,21 @@ customer_service = CustomerService()
 @cc.route('/')
 def index():
     endpoints_description = "How to use this API" + "\r\n" + "\r\n" + \
-        "'POST '/customers`: Creates a new customer" + "\r\n" + \
-        "`GET /customers`: Gets all customers" + "\r\n" +\
-        "`GET /customer/{customer_id}`: Get customer with an id of X (if the customer exists)" + "\r\n" +\
-        "`PUT /customer/{customer_id}`: Update customer with an id of X (if the customer exists)" + "\r\n" +\
-        "`DELETE /customer/{customer_id}`: Delete customer with an id of X (if the customer exists)" + "\r\n" +\
-        "`POST /customer/{customer_id}/accounts`: Create a new account for a customer with id of X (if customer " \
-        "exists)" + "\r\n" + \
-        "'GET /customer/{customer_id}/accounts?amountLessThan=1000&amountGreaterThan=300`: Get all accounts" \
-        " for customer id of X with balances between Y and Z (if customer exists)" + "\r\n" + \
-        "`GET /customer/{customer_id}/account/{account_id}`: Get account with id of Y belonging to the customer " \
-        "with id of X (if customer and account exist AND if account belongs to customer)" + "\r\n" +\
-        "`PUT /customer/{customer_id}/account/{account_id}`: Update account with id of Y belonging to customer" \
-        " with id of X (if customer and account exist AND if account belongs to customer)" + "\r\n" +\
-        "`DELETE /customer/{customer_id}/account/{account_id}`: Delete account with id of Y belonging to customer " \
-        "with id of X (if customer and account exist AND if account belongs to customer)"
+                            "'POST '/customers`: Creates a new customer" + "\r\n" + \
+                            "`GET /customers`: Gets all customers" + "\r\n" + \
+                            "`GET /customer/{customer_id}`: Get customer with an id of X (if the customer exists)" + "\r\n" + \
+                            "`PUT /customer/{customer_id}`: Update customer with an id of X (if the customer exists)" + "\r\n" + \
+                            "`DELETE /customer/{customer_id}`: Delete customer with an id of X (if the customer exists)" + "\r\n" + \
+                            "`POST /customer/{customer_id}/accounts`: Create a new account for a customer with id of X (if customer " \
+                            "exists)" + "\r\n" + \
+                            "'GET /customer/{customer_id}/accounts?amountLessThan=1000&amountGreaterThan=300`: Get all accounts" \
+                            " for customer id of X with balances between Y and Z (if customer exists)" + "\r\n" + \
+                            "`GET /customer/{customer_id}/account/{account_id}`: Get account with id of Y belonging to the customer " \
+                            "with id of X (if customer and account exist AND if account belongs to customer)" + "\r\n" + \
+                            "`PUT /customer/{customer_id}/account/{account_id}`: Update account with id of Y belonging to customer" \
+                            " with id of X (if customer and account exist AND if account belongs to customer)" + "\r\n" + \
+                            "`DELETE /customer/{customer_id}/account/{account_id}`: Delete account with id of Y belonging to customer " \
+                            "with id of X (if customer and account exist AND if account belongs to customer)"
 
     return endpoints_description
 
@@ -42,5 +43,21 @@ def get_customer_by_id(customer_id):
         return customer_service.get_customer_by_id(customer_id)  # dictionary
     except CustomerNotFoundError as e:
         return {
-            "message": str(e)
-        }, 404
+                   "message": str(e)
+               }, 404
+
+
+@cc.route('/api/customers', methods=['POST'])  # GET /api/customers/<customer_id>
+def add_customer():
+    cust = request.get_json()
+    customer = Customer(None, cust['first_name'], cust['last_name'],
+                        cust['date_of_birth'], None, cust['email'],
+                        cust['postal_code'], cust['unit_no'],
+                        cust['mobile_phone'])
+    try:
+        return customer_service.add_customer(customer), 201  # Dictionary representation of the newly added user
+        # 201 created
+    except InvalidParameterError as e:
+        return {
+                   "message": str(e)
+               }, 400
--- service/customer_service.py
+++ service/customer_service.py
@@ -1,5 +1,8 @@
+import re
+import utility.helpers
 from dao.customer_dao import CustomerDao
 from exception.customer_not_found import CustomerNotFoundError
+from exception.invalid_parameter import InvalidParameterError
 
 
 class CustomerService:
@@ -20,3 +23,22 @@ class CustomerService:
                                         f" {customer_id} was not found")
 
         return cust.to_dict()
+
+    def add_customer(self, cust):
+        reg_phone = r"[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        reg_email = r"[^@]+@[^@]+\.[^@]+"
+        if len(cust.get_first_name()) < 2 or len(cust.get_last_name()) < 2:
+            raise InvalidParameterError("Names must have at least 2 letters")
+        elif " " in cust.get_first_name() or " " in cust.get_last_name():
+            raise InvalidParameterError("Username cannot contain spaces")
+        elif not utility.helpers.check_date(cust.get_date_of_birth()):
+            raise InvalidParameterError("Minimum Customer age must be 16 years-old")
+        elif not re.match(reg_email, cust.get_email()):
+            raise InvalidParameterError("accepted email address format is <username>@<company>.<domain>")
+        elif not 4 < len(cust.get_postal_code()) < 7:
+            raise InvalidParameterError("Postal code length must be equal to 5 or 6")
+        elif not re.match(reg_phone, cust.get_mobile_phone()):
+            raise InvalidParameterError("mobile phone format 555-555-5555")
+
+        return self.customer_dao.add_customer(cust).to_dict()
+
