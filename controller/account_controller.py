b7a258de4236fcc47b605da2a7be297758d47df1
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-06-30 23:02:12 -0400

    add add_customer_by_id to controller layer, add an account controller blueprint in its own module

---
+++ controller/account_controller.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request
+from model.account import Account
+from service.account_service import AccountService
+from exception.customer_not_found import CustomerNotFoundError
+from exception.invalid_parameter import InvalidParameterError
+
+ac = Blueprint('account_controller', __name__)
+account_service = AccountService()
+
+
+@ac.route('/customer/{customer_id}/accounts', methods=['Post'])
+def add_account_by_customer_id(customer_id):
+    acc = request.get_json()
+    account = Account(None, acc['type_id'], acc['currency_id'], acc['balance'])
+    try:
+        return account_service.add_account_by_customer_id(account, customer_id), 201  # Dictionary representation of the newly added user
+        # 201 created
+    except InvalidParameterError as e:
+        return {
+                   "message": str(e)
+               }, 400
+    except CustomerNotFoundError as e:
+        return {
+                   "message": str(e)
+               }, 404
+
