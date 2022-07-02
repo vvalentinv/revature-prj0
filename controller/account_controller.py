8e24cf5bec2002382f47df5a5b6e1fcb262552b0
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-07-02 06:04:26 -0400

    add update_customer_account_by_account_id() to controller layer

--- controller/account_controller.py
+++ controller/account_controller.py
@@ -61,3 +61,21 @@ def get_customer_account_by_account_id(customer_id, account_id):
         return {
                    "message": str(e)
                }, 404
+
+
+@ac.route('/api/customers/<customer_id>/accounts/<account_id>', methods=['PUT'])
+def update_customer_account_by_account_id(customer_id, account_id):
+    try:
+        return account_service.update_customer_account_by_account_id(customer_id, account_id), 200
+    except UnauthorizedAccess as e:
+        return {
+                   "message": str(e)
+               }, 403
+    except CustomerNotFoundError as e:
+        return {
+                   "message": str(e)
+               }, 404
+    except AccountNotFound as e:
+        return {
+                   "message": str(e)
+               }, 404
